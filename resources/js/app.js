c735d3ca0683ffe39f147035fa1c94cc58c6fc76
Johnny Kamprath <johnny.kamprath@archrival.com>
2016-11-25 12:12:20 -0600

    Improve dashboard page
    * Made checklists render from JSON data
    * Added drag-and-drop functionality to todo list
    * Added background to page

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,6 +30,7 @@
      * Save todo items to localStorage
      */
     function saveTodo() {
+        console.log('saveTodo');
         var items = [];
 
         // iterate through todo items
@@ -55,7 +56,7 @@
     /**
      * Retrieve todo data from storage and populate list
      */
-    function loadTodo() {
+    function renderTodo() {
         var template = $('#todo-template').html();
 
         // fetch or create todoItems from localStorage
@@ -80,40 +81,50 @@
             $template.find('input[type=text]').val(items[i].text);
             $template.find('input[type=checkbox]').prop('checked', items[i].checked);
 
-            // insert element before 'add...' link
-            $('#create-todo').before($template);
+            $('.todo-list').append($template);
+
+            $('.todo-list').sortable({
+                onDrop: function ($item, container, _super, event) {
+                    $item.removeClass(container.group.options.draggedClass).removeAttr("style");
+                    $("body").removeClass(container.group.options.bodyClass);
+                    
+                    saveTodo();
+                }
+            });
         }
     }
 
-    function bindSaveEvent() {
-        $('.todo .input-group input')
-            .unbind('change')
-            .on('change', saveTodo);
+    /**
+     * Replace template placeholders with data from corresponding object keys
+     * 
+     * @param {string} html Template html
+     * @param {object} data An object of data containing keys that correspond with 
+     *                      template placeholders
+     * @returns {string}    Returns a string of rendered HTML
+     */
+    function renderTemplate(html, data) {
+        // iterate through keys in data
+        for (var key in data) {
+            // replace occurences of the key with its value
+            html = html.replace('{' + key + '}', data[key]);
+        }
+        
+        // return html
+        return html;
     }
 
-    // reset checkboxes and list styles when 'reset' is clicked
-    $('.reset').on('click', resetList);
-
-    // apply 'complete' style when a list box is clicked
-    $('.list-group-item').on('click', completeItem);
-
-    // don't check checkboxes when clicked because link will handle it
-    $('input[type=checkbox]').on('click', function(e) {
+    function createTodo(e) {
         e.preventDefault();
-    });
-
-    $('#create-todo').on('click', function(e) {
-        e.preventDefault();
-        
+            
         var $item =$($('#todo-template').html());
 
-        $(this).before($item);
+        $('.todo-list').append($item);
         $item.find('input[type=text]').focus();
-        
-        bindSaveEvent();
-    });
 
-    $('#clear-todo').on('click', function(e) {
+        bindTodoSave('.todo .input-group input', saveTodo);
+    }
+
+    function clearTodo(e) {
         e.preventDefault();
 
         var $items = $(this).parents('.todo').find('.input-group');
@@ -125,10 +136,79 @@
         });
 
         saveTodo();
-    });
+    }
+
+    function bindTodoSave(selector, fn) {
+        console.log('fucking retarded');
+        $(selector)
+            .unbind('change keyup')
+            .on('change keyup', fn)
+    }
+
+    function bindEvents() {
+        // reset checkboxes and list styles when 'reset' is clicked
+        $('.reset').on('click', resetList);
+
+        // apply 'complete' style when a list box is clicked
+        $('.list-group-item').on('click', completeItem);
+
+        // don't check checkboxes when clicked because link will handle it
+        $('.list-group-item input[type=checkbox]').on('click', function(e) {
+            e.preventDefault();
+        });
+
+        // create new todo item element when link is clicked
+        $('#create-todo').on('click', createTodo);
+
+        // remove completed todo items when link is clicked
+        $('#clear-todo').on('click', clearTodo);
+
+        // save todo data when a todo item is changed
+        bindTodoSave('.todo .input-group input', saveTodo);
+    }
+
+    /**
+     * Render checklists from data
+     */
+    function renderChecklists(data) {
+        var checklistTemplate = $('#checklist-template').html();
+        var checklistItemTemplate = $('#checklist-item-template').html();
+
+        for (var title in data) {
+            // create a new element to contain checklist
+            var $checklist = $(
+                renderTemplate(checklistTemplate, {'title': title})
+            );
+
+            // render template for each item and append to checklist element
+            for (var i in data[title]) {
+                // set 'number' field in item data
+                data[title][i].number = parseInt(i) + 1;
+
+                var itemHtml = renderTemplate(
+                    checklistItemTemplate, data[title][i]
+                );
+
+                $checklist.append(
+                    $(itemHtml)
+                );
+            }
+
+            // append checklist element to .checklists element
+            $('.checklists').append($checklist);
+        }
+    }
 
     $(document).ready(function() {
-        loadTodo();
-        bindSaveEvent();
+        // render todo items from localStorage
+        renderTodo();
+
+        // render checklists from JSON data
+        renderChecklists(
+            JSON.parse($('#checklist-json').html())
+        );
+
+        // bind events
+        bindEvents();
     });
 })();
