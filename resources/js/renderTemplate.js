35ecc73b8e0060f2317201c349df5bd2ad29a2db
Johnny Kamprath <johnny.kamprath@archrival.com>
2016-11-28 12:16:39 -0600

    Move functionality from app.js into views

---
+++ resources/js/renderTemplate.js
@@ -0,0 +1,18 @@
+/**
+ * Replace template placeholders with data from corresponding object keys
+ * 
+ * @param {string} html Template html
+ * @param {object} data An object of data containing keys that correspond with 
+ *                      template placeholders
+ * @returns {string}    Returns a string of rendered HTML
+ */
+module.exports = function(html, data) {
+    // iterate through keys in data
+    for (var key in data) {
+        // replace occurences of the key with its value
+        html = html.replace('{' + key + '}', data[key]);
+    }
+    
+    // return html
+    return html;
+}
