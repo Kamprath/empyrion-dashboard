f6fb1d2e4593388c9f65f6abe2a085dd387ae060
Johnny Kamprath <johnny.kamprath@archrival.com>
2016-11-28 12:15:24 -0600

    Setup gulp w/ Browserify + Uglify tasks

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var browserify = require('browserify'),
+    gulp = require('gulp'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gutil = require('gulp-util');
+
+/**
+ * Browserify, uglify, and produce source maps
+ */
+gulp.task('build', function() {
+    // set up browserify instance on a task basis
+    var b = browserify({
+        entries: './resources/js/app.js',
+        debug: true
+    });
+
+    return b.bundle()
+        // prepare new file
+        .pipe(
+            source('app.min.js')
+        ).pipe(buffer())
+
+        // create sourcemaps
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+
+        // uglify
+        .pipe(uglify())
+        .on('error', gutil.log)
+
+        // write sourcemaps
+        .pipe(
+            sourcemaps.write('./resources/js')
+        )
+
+        // write output to file
+        .pipe(
+            gulp.dest('./resources/js/')
+        );
+});
+
+gulp.task('default', ['build']);
