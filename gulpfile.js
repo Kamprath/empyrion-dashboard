39e26339c481521bee48da45ec5a189d98765662
Johnny Kamprath <johnny.kamprath@archrival.com>
2016-12-28 19:17:58 -0600

    Add JS lint task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,33 @@
-'use strict';
-
 var browserify = require('browserify'),
     gulp = require('gulp'),
     source = require('vinyl-source-stream'),
     buffer = require('vinyl-buffer'),
     uglify = require('gulp-uglify'),
     sourcemaps = require('gulp-sourcemaps'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
+
+/**
+ * Lint unminified JS files using jshint
+ */
+gulp.task('lint', function() {
+    return gulp.src([
+        '!./resources/js/*.min.js',
+        './resources/js/*.js', 
+        './resources/js/**/*.js'
+    ])
+        .pipe(jshint({
+            strict: false
+        }))
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'))
+});
 
 /**
  * Browserify, uglify, and produce source maps
  */
-gulp.task('build', function() {
+gulp.task('build', ['lint'], function() {
     // set up browserify instance on a task basis
     var b = browserify({
         entries: './resources/js/app.js',
